7729a1d1f4cd679ad857d34d7f236262a97072e4
YanaD25 <yanaderevianko25@gmail.com>
2020-05-19 22:48:53 +0300

    alert

--- JS/hw-01/task-03.js
+++ JS/hw-01/task-03.js
@@ -7,9 +7,10 @@ if (message === null) {
     console.log (`Отменено пользователем!`);
 }else if (message === ADMIN_PASSWORD) {
     console.log (`Добро пожаловать!`);
+    alert (message);
 }else if (message !== ADMIN_PASSWORD) {
     console.log (`Доступ запрещен, неверный пароль!`);
+    alert (message);
 }
-alert (message);
 
 
