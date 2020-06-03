f47de3b8c6bd763a44a4f5fde6e7e1413a3fc281
YanaD25 <yanaderevianko25@gmail.com>
2020-06-03 21:52:06 +0300

    task-06

--- module-02/hw-02/task-06.js
+++ module-02/hw-02/task-06.js
@@ -3,15 +3,17 @@ const numbers = [];
 let total = 0;
 
 do {
-    input = prompt (`Введите число`);
-    if (Number.isNaN(+input)) {
-        alert(`Было введено не число, попробуйте еще раз`);
-        continue;
-}
-numbers.push(+input);
-} while (input !== null)
-for (let number of numbers) {
-    total += number;
-}
+  input = prompt(`Введите число`);
+  if (Number.isNaN(+input)) {
+    alert(`Было введено не число, попробуйте еще раз`);
+    continue;
+  }
+  numbers.push(+input);
+} while (input !== null);
 
+if (numbers.lenght !== 0) {
+  for (let number of numbers) {
+    total += number;
+  }
+}
 console.log(`Общая сумма чисел равна: ${total}`);
