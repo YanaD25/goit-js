750a618148e6acc35c5559fc61eb58f4d59b2bfb
YanaD25 <yanaderevianko25@gmail.com>
2020-06-28 14:16:45 +0300

    task 02

--- module-06/hw-06/tasks.js
+++ module-06/hw-06/tasks.js
@@ -6,3 +6,11 @@ const getUserNames = (users) => users.map((user) => user.name);
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+// TASK-02
+const getUsersWithEyeColor = (users, color) => users.map((user) => user.eyeColor);
+    // твой код
+
+  
+  console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
