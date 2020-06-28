44ca3ecf79b5a6952416362d8836d6700ef70e10
YanaD25 <yanaderevianko25@gmail.com>
2020-06-28 14:05:10 +0300

    11

---
+++ module-06/hw-06/tasks.js
@@ -0,0 +1,8 @@
+import { users } from "./users.js";
+
+// TASK-01
+const getUserNames = (users) => users.map((user) => user.name);
+// твой код
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
