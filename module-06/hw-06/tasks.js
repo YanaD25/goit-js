33f948ee16235657c327466e3cacd95e16e6135c
YanaD25 <yanaderevianko25@gmail.com>
2020-06-28 16:40:14 +0300

    333

--- module-06/hw-06/tasks.js
+++ module-06/hw-06/tasks.js
@@ -7,10 +7,19 @@ const getUserNames = (users) => users.map((user) => user.name);
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-
 // TASK-02
-const getUsersWithEyeColor = (users, color) => users.filter(user => color === user.eyeColor);
-    // твой код
+const getUsersWithEyeColor = (users, color) =>
+  users.filter((user) => color === user.eyeColor);
+// твой код
 
-  
-  console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+// TASK-03
+const getUsersWithGender = (users, gender) => {
+  return users
+    .filter((user) => gender === user.gender)
+    .map((user) => user.name);
+  // твой код
+};
+
+console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
