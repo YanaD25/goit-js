3c5ec4ac7e1e1c11a65f949c82a92cb48f380c3c
YanaD25 <yanaderevianko25@gmail.com>
2020-08-24 18:54:09 +0300

    switch color

---
+++ hw-11-color-switch/src/index.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+
+
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stopBtnRef = document.querySelector('button[data-action="stop"]');
+
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",  
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+const generateRandomColor = {
+  isActive: false,
+  intervalId: null,
+  start() {
+  if (this.isActive) {
+    return
+  }
+this.isActive = true;
+this.intervalId = setInterval(() => {
+  document.body.style = `background-color: ${colors[randomIntegerFromInterval(0, colors.length - 1)]};`;},1000
+);
+  },
+stop() {
+  clearInterval(this.intervalId);
+  this.intervalId = null;
+  this.isActive = false;
+},
+};
+
+startBtnRef.addEventListener('click', generateRandomColor.start.bind(generateRandomColor));
+stopBtnRef.addEventListener('click', generateRandomColor.stop.bind(generateRandomColor));
